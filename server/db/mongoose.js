6796cc09ebdcaba8508bd9c2a0629b0c5f3aea95
Abhishek <abhishekchadha37@yahoo.in>
2017-09-23 15:19:25 +0530

    status updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
 //mongoose.connect(process.env.MONGODB_URI||mongodb:abhishekchadha37:Abhi94632@ds147454.mlab.com:47454/todoapp)
 module.exports={mongoose}
