6e0a3c203e444671025a29a6b8368b1e17eda8dc
Abhishek <abhishekchadha37@yahoo.in>
2017-09-23 15:56:31 +0530

    Updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp', { useMongoClient: true }).then(()=>{
+
+}).catch(err=>{
+  console.error('App starting error:', err.stack);
+        process.exit(1);
+});
 //mongoose.connect(process.env.MONGODB_URI||mongodb:abhishekchadha37:Abhi94632@ds147454.mlab.com:47454/todoapp)
 module.exports={mongoose}
