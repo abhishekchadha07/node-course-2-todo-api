c2a5c3c4a02afd3d9cd388a7bf689879afa0e2d4
Abhishek <abhishekchadha37@yahoo.in>
2017-09-23 22:34:18 +0530

    Test the delete todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -114,3 +114,45 @@ request(app)
     .end(done);
   })
 })
+
+
+describe('Delete / todos /:id',()=>{
+  it('should remove a todo',(done)=>{
+var hexid=todos[1]._id.toHexString();
+request(app)
+.delete(`/todos/${hexid}`)
+.expect(200)
+.expect((res)=>{
+  expect(res.body.todo._id).toBe(hexid)
+})
+.end((err,res)=>{
+  if(err){
+    return done(err);
+  }
+
+  Todo.findById(hexid).then((todo)=>{
+    expect(todo).toNotExist();
+    done();
+  }).catch((e)=>done(e));
+})
+  })
+it('should return 404 if todo not found',(done)=>{
+ request(app)
+  .delete(`/todos/${new ObjectID().toHexString()}`)
+  .expect(400)
+  // .expect((res)=>{
+  //   expect(res.body.todo.length).toBe(1);
+  // })
+  .end(done);
+})
+//
+it('should return 404 if object id is invalid',(done)=>{
+  request(app)
+  .get(`/todos/qq`)
+  .expect(404)
+  // .expect((res)=>{
+  //   expect(res.body.todo.length).toBe(1);
+  // })
+  .end(done);
+})
+})
